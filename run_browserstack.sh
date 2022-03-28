9614b9e91be454c36123c0edae4773cf57ed9c93
Roberto D'Auria <roberto@measurementlab.net>
2022-03-28 15:58:11 +0200

    Move testcafe to a script and fix loadbalancer url

---
+++ run_browserstack.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script runs the e2e tests on browserstack.
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+set -x # Print commands and their arguments as they are executed.
+
+# Define an array of browsers to run.
+BROWSERS_WINDOWS=(
+    "browserstack:chrome@99.0:Windows 11"
+    "browserstack:opera@84.0:Windows 11"
+    "browserstack:edge@99.0:Windows 11"
+    "browserstack:firefox@98.0:Windows 11"
+)
+
+BROWSERS_MACOS_SAFARI=(
+    "browserstack:safari@15.3:OS X Monterey"
+    "browserstack:safari@14.1:OS X Big Sur"
+    "browserstack:safari@13.1:OS X Catalina"
+    "browserstack:safari@12.1:OS X Mojave"
+    "browserstack:safari@11.1:OS X High Sierra"
+)
+
+BROWSERS_MACOS_OTHERS=(
+    "browserstack:chrome@99.0:OS X Monterey"
+    "browserstack:opera@84.0:OS X Monterey"
+    "browserstack:edge@99.0:OS X Monterey"
+    "browserstack:firefox@98.0:OS X Monterey"
+)
+
+function run_tests() {
+    BROWSER_LIST=""
+    BROWSERS=("$@")
+    for ((i = 0; i < ${#BROWSERS[@]}; i++)); do
+        BROWSER_LIST="${BROWSER_LIST},\"${BROWSERS[i]}\""
+    done
+    
+    # Remove the initial comma.
+    BROWSER_LIST=${BROWSER_LIST:1}
+
+    testcafe "$BROWSER_LIST" src/test/e2e/test.js --app "node src/test/e2e/server.js"
+}
+
+run_tests "${BROWSERS_MACOS_SAFARI[@]}"
