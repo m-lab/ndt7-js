f71d6ac4cf21dadf7805880ee6f38ff02c0c7c8a
Roberto D'Auria <roberto@measurementlab.net>
2022-04-01 09:27:21 +0200

    Configure BrowserStack-based testing (#62)
    * Run "npm run test" in travis
    
    * Add browserstack script to package.json
    
    * Use staging servers for testing.
    
    * Add browserstack plugin to testcafe.
    
    * Fix path in package.json.
    
    * Move testcafe to a script and fix loadbalancer url
    
    * Add windows and macos_others browsers
    
    * Use travis's own browserstack integration
    
    * Update credentials.
    
    * plaintext username
    
    * Fix indent.
    
    * Add yaml version to .travis.yml
    
    * Move sections around in .travis.yml
    
    * testing with global env var
    
    * test
    
    * test again
    
    * Disable Safari tests.
    
    * Run single Safari test
    
    * Update TestCafe and re-enable safari tests
    
    * Run one test at a time.
    
    * Run all tests
    
    * Run multiple instances of testcafe
    
    This should help with the timeouts seen on MacOS.
    Also, fix postMessage on the websocket onerror callbacks.
    
    * Run with Automate
    
    * Improve script
    
    * Add networkLogs: true
    
    * Terminate node and BrowserStackLocal
    
    * Update config and set browserstack to run via cronjob
    
    * Move browserstack plugin to devDependencies.
    
    * Update comment.
    
    * Merge branch 'main' into sandbox-roberto-testing
    
    * Encrypt username, too.
    
    Not required by the documentation, but nice.
    
    * Merge branch 'main' into sandbox-roberto-testing

--- src/test/e2e/test.js
+++ src/test/e2e/test.js
@@ -10,17 +10,17 @@ const uploadStatus = Selector('#uploadStatus');
 const upload = Selector('#upload');
 
 test('Basic functionality tests', async t => {
-    // The assertion timeout is set to 5 seconds, so notEql() will wait until
+    // The assertion timeout is set to 10 seconds, so notEql() will wait until
     // the server div becomes non-empty. This should be plenty of time for
     // Locate to return a server, but there is no guarantee. If that does not
     // happen it indicates a problem with either the testing machine or the
     // Locate service. In both cases, the test should fail.
-    await t.expect(server.innerText).notEql('', { timeout: 5000 });
+    await t.expect(server.innerText).notEql('', { timeout: 10000 });
 
     // We're in the middle of the download test, so these divs should be
-    // populated. A 5-second timeout is used here as well, to handle cases
+    // populated. A 10-second timeout is used here as well, to handle cases
     // where connecting to the server takes longer than usual.
-    await t.expect(downloadStatus.innerText).eql('measuring', { timeout: 5000 });
+    await t.expect(downloadStatus.innerText).eql('measuring', { timeout: 10000 });
     await t.expect(download.innerText).notEql('');
 
     // The download test takes 10 seconds, so we expect downloadStatus to
@@ -28,14 +28,17 @@ test('Basic functionality tests', async t => {
     await t.expect(downloadStatus.innerText).eql('complete', { timeout: 15000 });
     await t.expect(download.innerText).notEql('');
 
-    // The same 5-second timeout as above is used for the client to connect
+    // The same 10-second timeout as above is used for the client to connect
     // for the upload test.
-    await t.expect(uploadStatus.innerText).eql('measuring', { timeout: 5000 });
+    await t.expect(uploadStatus.innerText).eql('measuring', { timeout: 10000 });
     await t.expect(upload.innerText).notEql('');
 
     // The upload test takes 10 seconds, so we expect uploadStatus to
     // become "complete" within 15s at most.
     await t.expect(uploadStatus.innerText).eql('complete', { timeout: 15000 });
     await t.expect(upload.innerText).notEql('');
+
+    // The error div should be empty.
+    await t.expect(Selector('#error').innerText).eql('');
 
 });
